67bb7b5e9cbaafb11fa42459fd2d77df26b7f5f4
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2025-09-11 17:33:55 +0200

    remove keepAlive for now

--- src/reverseterminal/widget.ts
+++ src/reverseterminal/widget.ts
@@ -135,22 +135,6 @@ export class LazyTerminal extends Widget implements ITerminal.ITerminal {
     }
   }
 
-  private _startKeepAlive(): void {
-    if (!this.session) {
-      return;
-    }
-    setInterval(() => {
-      if (this.session?.connectionStatus === 'connected') {
-        try {
-          // send a benign no-op control message
-          this.session?.send({ type: 'stdin', content: [''] });
-        } catch (e) {
-          console.warn('Keepalive failed', e);
-        }
-      }
-    }, 8000);
-  }
-
   private _waitForTerminal(system: string) {
     retrieveShell(system, data => this.printStatus(data));
   }
@@ -389,8 +373,6 @@ export class LazyTerminal extends Widget implements ITerminal.ITerminal {
       this._initialConnection,
       this
     );
-
-    this._startKeepAlive();
   }
 
   /**
